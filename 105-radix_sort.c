ac7c207c192175d414d647d450e00ad4b67d2afc
Joseph Ofori <josephofori75@gmail.com>
2023-12-20 02:20:28 +0000

    advance task

---
+++ 105-radix_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+/**
+ * radix_sort - sorts array in ascending
+ * order using the Radix sort algorithm
+ *
+ * @array: input array
+ * @size: size of the array
+ */
+void radix_sort(int *array, size_t size)
+{
+  int flags = 1, n = 10;
+  size_t i, f;
+
+  if (!array || size == 1)
+    return;
+  while (flags)
+    {
+      flags = 0;
+      for (i = 1, f = 1; i <  size; i++, f++)
+	{
+	  if ((array[i - 1] % (n * 10)) / ((n * 10) / 10) > 0)
+	    flags = 1;
+	  if (((array[i - 1] % n) / (n / 10)) > ((array[i] % n) / (n / 10)))
+	    {
+	      array[i - 1] = array[i - 1] + array[i];
+	      array[i] = array[i - 1] - array[i];
+	      array[i - 1] = array[i - 1] - array[i];
+	      if ((i - 1) > 0)
+		i = i - 2;
+	    }
+	}
+      print_array(array, size);
+      n = n * 10;
+    }
+}
