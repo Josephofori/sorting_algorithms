7bc95f19488068e2e9b9ffcdaebb928ed44a1255
Joseph Ofori <josephofori75@gmail.com>
2023-12-20 02:27:10 +0000

    advance task

---
+++ 100-shell_sort.c
@@ -0,0 +1,46 @@
+#include "sort.h"
+/**
+ * shell_sort - function which sorts array
+ * of integers in ascending order with the
+ * Shell sort algorithm
+ *
+ * @array: arrray input
+ * @size: size of array
+ * Return: none
+ */
+void shell_sort(int *array, size_t size)
+{
+  size_t m = 1;
+
+  while (m < size)
+    m = (m * 3) + 1;
+
+  while ((m = (m - 1) / 3) > 0)
+    _shsort(array, size, n);
+}
+/**
+ * _shsort - auxiliar function for
+ * shell_sort function
+ * @a: arrray input
+ * @size: size of array
+ * @n: intervale
+ * Return: none
+ */
+void _shsort(int *a, int size, int n)
+{
+  int temp, i, j;
+
+  for (i = 0; (i + n) < size; i++)
+    {
+      for (j = i + n; (j - n) >= 0; j = j - n)
+	{
+	  if (a[j] < a[j - n])
+	    {
+	      temp = a[j];
+	      a[j] = a[j - n];
+	      a[j - n] = temp;
+	    }
+	}
+    }
+  print_array(a, size);
+}
--- sort.h
+++ sort.h
@@ -25,7 +25,7 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void _qsort(int *a, int low, int high, int size);
 void shell_sort(int *array, size_t size);
-void _ssort(int *a, int size, int n);
+void _shsort(int *a, int size, int n);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
